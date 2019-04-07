2d9d37bb2bff99c8a48fbb922ac89c55eef0f914
csporto23 <Csporto23@gmail.com>
2019-04-06 22:29:22 -0500

    javascript logic

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -7,15 +7,50 @@ var config = {
     storageBucket: "rps-multiplayer-d49f3.appspot.com",
     messagingSenderId: "164457531538"
   };
+
   firebase.initializeApp(config);
   var database = firebase.database();
 
-  $("#new-game").on("click", function(){
-      $(".container").css("display", "block")
-      $("#startButton").closest('div').remove();
-  })
 
+  //if person clicks new game
+  $("#new-game").on("click", function(){
+      const playerOne = prompt("player one:", "UserName")
+      $("#userNameOne").text(playerOne);
+      $(".container").css("display", "block");
+      $("#startButton").closest('div').remove();
+      var idNumber = Math.floor(Math.random() * 300);
+      var gameIdDiv = $("<div id='idNumber' class='text-white'>").text(idNumber)
+      $("#game-id").append(gameIdDiv)
+  });
+
+
+  //if person clicks join game
   $("#join-game").on("click", function(){
-    $(".container").css("display", "block")
+      prompt("game id:")
+      prompt("player two:")
+    $(".container").css("display", "block");
     $("#startButton").closest('div').remove();
-})
+});
+
+// const playerOne = $("")
+// const playerOne = $("")
+
+// var playerOneChoice = "";
+// var playerTwoChoice = "";
+
+// let pOneWins = 0;
+// let pOneLoses = 0;
+// let pTwoLoses = 0;
+// let pTwoWins = 0;
+
+// if(playerOne) {
+//     $("#rockOne").on("click", function() {
+//         playerOneChoice = "rock"
+//     });
+//     $("#paperOne").on("click", function() {
+//         playerOneChoice = "paper"
+//     });
+//     $("#scissorsOne").on("click", function() {
+//         playerOneChoice = "scissors"
+//     });
+// }
